25dac0fbde6c3a028c9e0cea532f3bfc560c9eef
thinhph <thinhph>
2021-12-21 14:22:46 +0700

    fix_project_name

--- template.config.js
+++ template.config.js
@@ -3,7 +3,7 @@
 module.exports = {
     // Placeholder used to rename and replace in files
     // package.json, index.json, android/, ios/
-    placeholderName: "projectname",
+    placeholderName: "ProjectName",
     // Directory with template
     templateDir: "./ProjectName"
 };
